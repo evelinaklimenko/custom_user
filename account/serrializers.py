b81ab0d9acee501a922796c15fddc50cb5dc6045
Evelina <klimenkoevelina1@gmail.com>
2023-01-09 12:37:03 +0600

    Custom_user

---
+++ account/serrializers.py
@@ -0,0 +1,54 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=4, required=True)
+    password_confirm = serializers.CharField(min_length=4, required=True)
+    name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь с таким email существует')
+        return email
+
+    def validate(self, attr):
+        password = attr.get('password')
+        password2 = attr.pop('password_confirm')
+        if password != password2:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return attr
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь не найден')
+        return email
+
+    def validate(self, data):
+        request = self.context.get('request')
+        email = data.get('email')
+        password = data.get('password')
+        if email and password:
+            user = authenticate(
+                username=email,
+                password=password,
+                requests=request
+            )
+            if not user:
+                raise serializers.ValidationError('Не верный email или пароль')
+        else:
+            raise serializers.ValidationError('Email и пароль обязательны к заполнению')
+        data['user'] = user
+        return data
---
+++ account/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serrializers import RegistrationSerializer, LoginSerializer
+
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Аккаунт успешно создан', status=201)
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+
